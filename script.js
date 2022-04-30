a0e57bca324a1242d05dbe014fc5f9741bb2be01
Ravina Dhamani <47536219+ravinadhamani@users.noreply.github.com>
2022-04-30 21:18:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-var url = 'http://www.filltext.com/?rows=32&id=%7Bnumber%7C1000%7D&firstName=%7BfirstName%7D&lastName=%7BlastName%7D&email=%7Bemail%7D&phone=%7Bphone%7C(xxx)xxx-xx-xx%7D&address=%7BaddressObject%7D&description=%7Blorem%7C32%7D';
-
+var url = 'https://codepen.io/ravinadhamani/pen/jOZOqpd'
 
 var tableData = document.getElementById("table-data1")
 var infoWrapper = document.getElementById("info-wrapper")
